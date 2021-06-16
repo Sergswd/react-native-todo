cae2d11db67e398d8a2379ab40ec19fa8a34dd97
Sergey Kh <falko.serega2016@yandex.ru>
2021-06-16 13:23:23 +0300

    refactoring an application

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -9,56 +9,13 @@ import { TodoScreen } from './screens/TodoScreen'
 import { THEME } from './theme'
 
 export const MainLayout = () => {
-  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext)
-  const { todoId, changeScreen } = useContext(ScreenContext)
-
-  // const removeTodo = (id) => {
-  //   const remTodo = todos.find(todo => todo.id === id)
-  //   Alert.alert(
-  //     "deleting an item",
-  //     `Are you sure you want to delete "${remTodo.title}"?`,
-  //     [
-  //       {
-  //         text: "Cancel",
-  //         style: "cancel"
-  //       },
-  //       { text: "Delete", 
-  //         onPress: () => {
-  //           setTodoId(null)
-  //           setTodos(prev => prev.filter(todo => todo.id !== id))
-  //         }, 
-  //         style: "destructive"
-  //       }
-  //     ]
-  //   );
-  // }
-
-  let content = (
-    <MainScreen 
-      todos={todos} 
-      addTodo={addTodo} 
-      removeTodo={removeTodo} 
-      openTodo={changeScreen}
-    />
-  )
-
-  if (todoId) {
-    const selectedTodo = todos.find(todo => todo.id === todoId)
-    content = (
-      <TodoScreen 
-        goBack={() => changeScreen(null)} 
-        todo={selectedTodo}
-        onRemove={removeTodo}
-        onSave={updateTodo}
-      />
-    )
-  }
+  const { todoId } = useContext(ScreenContext)
 
   return (
     <View>
       <Navbar text='Todo App'/>
       <View style={styles.container}>
-        {content}
+        {todoId ? <TodoScreen /> : <MainScreen />}
       </View>
       <StatusBar style="auto" />
     </View>
--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useContext } from 'react'
+import { Alert } from 'react-native'
 import { TodoContext } from './todoContext'
 import { todoReducer } from './todoReducer'
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
@@ -10,12 +11,31 @@ export const TodoState = ({ children }) => {
   }
 
   const { changeScreen } = useContext(ScreenContext)
-
   const [state, dispatch] = useReducer(todoReducer, initialState)
+
   const addTodo = title => dispatch({ type: ADD_TODO, title})
+
   const removeTodo = id => {
-    changeScreen(null)
-    dispatch({ type: REMOVE_TODO, id })
+    const todo = state.todos.find(t => t.id === id)
+    Alert.alert(
+      "deleting an item",
+      `Are you sure you want to delete "${todo.title}"?`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        { text: "Delete", 
+          onPress: () => {
+            changeScreen(null)
+            dispatch({ type: REMOVE_TODO, id })
+          }, 
+          style: "destructive"
+        }
+      ],
+      { cancelable: false }
+    );
+    
   }
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title})
 
